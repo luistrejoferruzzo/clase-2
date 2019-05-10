c45c751611936b1bcb03a7d67ba58886e6cd4d34
L.T.F <luistrejoferruzzo@hotmail.com>
2019-05-09 21:40:00 -0500

    Ejercicio calculadora

---
+++ app/src/main/java/com/example/hp/calculadora/MainActivity.java
@@ -0,0 +1,54 @@
+package com.example.hp.calculadora;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private EditText et1;
+    private EditText et2;
+    private TextView tv1;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        et1 = (EditText) findViewById(R.id.etNumero1);
+        et2 = (EditText) findViewById(R.id.etNumero2);
+        tv1 = (TextView) findViewById(R.id.txtResultado);
+    }
+
+    //este metodo realiza la suma
+    public void Sumar(View view) {
+        String valor1 = et1.getText().toString();
+        String valor2 = et2.getText().toString();
+
+        int num1 = Integer.parseInt(valor1);
+        int num2 = Integer.parseInt(valor2);
+
+        int suma = num1 + num2;
+
+        String result = String.valueOf(suma);
+        tv1.setText(result);
+
+    }
+
+    //este metodo realiza la resta
+    public void Restar(View view) {
+        String valor1 = et1.getText().toString();
+        String valor2 = et2.getText().toString();
+
+        int num1 = Integer.parseInt(valor1);
+        int num2 = Integer.parseInt(valor2);
+
+        int resta = num1 - num2;
+
+        String result = String.valueOf(resta);
+        tv1.setText(result);
+
+    }
+}
